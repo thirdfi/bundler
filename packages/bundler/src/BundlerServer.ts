f7e8e9ce3011b700f550450b4e3892b7d0b910d0
Siew <siewchongche@gmail.com>
2023-04-05 13:32:14 +0800

    fix: comment out debug mode rpc

--- packages/bundler/src/BundlerServer.ts
+++ packages/bundler/src/BundlerServer.ts
@@ -155,34 +155,34 @@ export class BundlerServer {
       case 'web3_clientVersion':
         result = this.methodHandler.clientVersion()
         break
-      case 'debug_bundler_clearState':
-        this.debugHandler.clearState()
-        result = 'ok'
-        break
-      case 'debug_bundler_dumpMempool':
-        result = await this.debugHandler.dumpMempool()
-        break
-      case 'debug_bundler_setReputation':
-        await this.debugHandler.setReputation(params[0])
-        result = 'ok'
-        break
-      case 'debug_bundler_dumpReputation':
-        result = await this.debugHandler.dumpReputation()
-        break
-      case 'debug_bundler_setBundlingMode':
-        await this.debugHandler.setBundlingMode(params[0])
-        result = 'ok'
-        break
-      case 'debug_bundler_setBundleInterval':
-        await this.debugHandler.setBundleInterval(params[0], params[1])
-        result = 'ok'
-        break
-      case 'debug_bundler_sendBundleNow':
-        result = await this.debugHandler.sendBundleNow()
-        if (result == null) {
-          result = 'ok'
-        }
-        break
+      // case 'debug_bundler_clearState':
+      //   this.debugHandler.clearState()
+      //   result = 'ok'
+      //   break
+      // case 'debug_bundler_dumpMempool':
+      //   result = await this.debugHandler.dumpMempool()
+      //   break
+      // case 'debug_bundler_setReputation':
+      //   await this.debugHandler.setReputation(params[0])
+      //   result = 'ok'
+      //   break
+      // case 'debug_bundler_dumpReputation':
+      //   result = await this.debugHandler.dumpReputation()
+      //   break
+      // case 'debug_bundler_setBundlingMode':
+      //   await this.debugHandler.setBundlingMode(params[0])
+      //   result = 'ok'
+      //   break
+      // case 'debug_bundler_setBundleInterval':
+      //   await this.debugHandler.setBundleInterval(params[0], params[1])
+      //   result = 'ok'
+      //   break
+      // case 'debug_bundler_sendBundleNow':
+      //   result = await this.debugHandler.sendBundleNow()
+      //   if (result == null) {
+      //     result = 'ok'
+      //   }
+      //   break
       default:
         throw new RpcError(`Method ${method} is not supported`, -32601)
     }
